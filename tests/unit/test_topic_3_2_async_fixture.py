850bb0d0d5552396d3ad09bf05dacdbab63346de
Chirag Jain <cjain9975@gmail.com>
2025-12-15 09:05:35 +0530

    Python PytTest Practice Examples

--- tests/unit/test_topic_3_2_async_fixture.py
+++ tests/unit/test_topic_3_2_async_fixture.py
@@ -1,6 +1,9 @@
 import pytest
 from app.async_resource import AsyncResource
 
+# this is async fixture implementation
+# here scope default is function level
+# Async fixtures allow setup and teardown of async resources
 @pytest.fixture
 async def async_resource():
     # intitalize the setup with constructor
@@ -12,6 +15,7 @@ async def async_resource():
     # destroying the setup
     await resource.close()
 
+# this function is marked as asyncio because
 @pytest.mark.asyncio
 async def test_async_resource_lifecycle(async_resource):
     assert async_resource.connected is True
