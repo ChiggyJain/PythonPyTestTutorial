4a6fe0f9b7b00eb628c6eefe983c67338010d0c6
Chirag Jain <cjain9975@gmail.com>
2025-12-15 07:57:15 +0530

    Python PytTest Practice Examples

--- app/currency.py
+++ app/currency.py
@@ -2,6 +2,11 @@
 from app.external_service import fetch_exchange_rate
 
 
+# Direct Import
+# here func: fetch_exchange_rate is imported during the import statement and its stored
+# into app.currency level as reference level
+# Creates a local reference
+# Copies fetch_exchange_rate into currency.py
 def convert_usd_to_inr(usd_amount: float) -> float:
     rate = fetch_exchange_rate()
     return usd_amount * rate
